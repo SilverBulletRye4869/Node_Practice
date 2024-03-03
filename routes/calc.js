9239874de2ebff2782712ff68557a024455b645c
SilverBullet_Rye <silverbulletrye4869@gmail.com>
2024-03-03 16:39:47 +0900

    ログイン状況に応じて表示が変わるように

--- routes/calc.js
+++ routes/calc.js
@@ -1,16 +1,25 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/",(req,res)=>{
-    res.render('calc', { a:"", b:"", calc_res: ""});
+router.get("/", (req, res) => {
+  res.render("calc", {
+    user: req.user !== undefined ? req.user.username : undefined,
+    a: "",
+    b: "",
+    calc_res: "",
+  });
 });
 
-router.post('/', (req, res) => {
-    const a = req.body.a;
-    const b = req.body.b;
-    const c = parseInt(a)+parseInt(b);
-    res.render('calc', { a:a, b:b, calc_res: c.toString() });
-    
+router.post("/", (req, res) => {
+  const a = req.body.a;
+  const b = req.body.b;
+  const c = parseInt(a) + parseInt(b);
+  res.render("calc", {
+    user: req.user !== undefined ? req.user.username : undefined,
+    a: a,
+    b: b,
+    calc_res: c.toString(),
+  });
 });
 
 module.exports = router;
